a74d2210e634d00b64d0094dccac55942596ff1f
Georg Kiviväli <georg.kivivali@uptime.eu>
2022-11-30 15:24:47 +0200

    added executemethod callback

--- sdkjs-plugins/content/form2db/scripts/code.js
+++ sdkjs-plugins/content/form2db/scripts/code.js
@@ -1,7 +1,9 @@
 ((window, undefined) => {
     window.Asc.plugin.init = () => {
         console.log('I run');
-        window.Asc.plugin.executeMethod('GetAllForms');
+        window.Asc.plugin.executeMethod('GetAllForms', (res) => {
+            console.log(res);
+        });
 
         // console.log(JSON.stringify(allForms));
     };
