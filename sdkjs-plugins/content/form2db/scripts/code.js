7ab973a99d5da247f509bd392cb8c02a5c0bc8f7
Georg Kiviväli <georg.kivivali@uptime.eu>
2022-11-30 15:28:08 +0200

    added onmethodreturn

--- sdkjs-plugins/content/form2db/scripts/code.js
+++ sdkjs-plugins/content/form2db/scripts/code.js
@@ -1,10 +1,10 @@
 ((window, undefined) => {
     window.Asc.plugin.init = () => {
         console.log('I run');
-        window.Asc.plugin.executeMethod('GetAllForms', (res) => {
-            console.log(res);
-        });
-
+        window.Asc.plugin.executeMethod('GetAllForms');
+        window.Asc.plugin.onMethodReturn = () => {
+            console.log("Running from onmethodreturn", window.Asc.plugin.info)
+        }
         // console.log(JSON.stringify(allForms));
     };
 })(window, undefined);
