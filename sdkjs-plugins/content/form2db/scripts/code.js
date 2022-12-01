0befb182c9f238736dfb71d20455b20285760a87
Georg Kiviväli <georg.kivivali@uptime.eu>
2022-12-01 13:21:33 +0200

    added request to temporary requestbin

--- sdkjs-plugins/content/form2db/scripts/code.js
+++ sdkjs-plugins/content/form2db/scripts/code.js
@@ -9,8 +9,24 @@
             if (data) {
                 for (const id of data) {
                     ids.push(id.InternalId);
-                    console.log(id.InternalId)
-                    window.Asc.plugin.executeMethod('GetFormValue', [id.InternalId], (value) => console.log("value is ", value));
+                    console.log(id.InternalId);
+                    window.Asc.plugin.executeMethod(
+                        'GetFormValue',
+                        [id.InternalId],
+                        (value) => {
+                            console.log('value is ', value);
+
+                            if (value) {
+                                const result = fetch(
+                                    'https://eosuz6runlql9t9.m.pipedream.net',
+                                    {
+                                        method: 'POST',
+                                        body: JSON.stringify(value),
+                                    }
+                                ).then((res) => console.log(res.status));
+                            }
+                        }
+                    );
                 }
             }
         };
