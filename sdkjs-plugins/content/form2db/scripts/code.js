4c878031a7a06c3f4f41788bdf5a706087b42f5f
Georg Kiviväli <georg.kivivali@uptime.eu>
2022-12-01 09:58:05 +0200

    are you working?

--- sdkjs-plugins/content/form2db/scripts/code.js
+++ sdkjs-plugins/content/form2db/scripts/code.js
@@ -1,12 +1,10 @@
 ((window, undefined) => {
     window.Asc.plugin.init = () => {
         console.log('I run');
-        const allForms = window.Asc.plugin.onMethodReturn = () => {
+        window.Asc.plugin.onMethodReturn = () => {
             window.Asc.plugin.executeMethod('GetAllForms');
 
-
-            console.log("Running from onmethodreturn", window.Asc.plugin.info)
-        }
-        console.log(JSON.stringify(allForms));
+            console.log('Running from onmethodreturn', window.Asc.plugin.info);
+        };
     };
 })(window, undefined);
