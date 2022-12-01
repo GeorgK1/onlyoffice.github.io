41e5ab5f83bb623d30595a00ad8f19a2fc7e2e21
Georg Kiviväli <georg.kivivali@uptime.eu>
2022-12-01 10:25:57 +0200

    changes

--- sdkjs-plugins/content/form2db/scripts/code.js
+++ sdkjs-plugins/content/form2db/scripts/code.js
@@ -1,10 +1,13 @@
 ((window, undefined) => {
     window.Asc.plugin.init = () => {
         console.log('I run');
-        window.Asc.plugin.onMethodReturn = () => {
-            window.Asc.plugin.executeMethod('GetAllForms');
-
-            console.log('Running from onmethodreturn', window.Asc.plugin.info);
-        };
+        const oDocument = Api.GetDocument();
+        const allForms = oDocument.getAllForms();
+        console.log(allForms);
+        
+        // window.Asc.plugin.executeMethod('GetAllForms');
+        // window.Asc.plugin.onMethodReturn = () => {
+        //     console.log("Running from onmethodreturn", window.Asc.plugin.info)
+        // }
     };
 })(window, undefined);
