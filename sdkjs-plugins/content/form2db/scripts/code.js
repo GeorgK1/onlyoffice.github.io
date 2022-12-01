8768348ad95a330e54f210d135ec02cdfd0f916a
Georg Kiviväli <georg.kivivali@uptime.eu>
2022-12-01 11:28:06 +0200

    implemented code

--- sdkjs-plugins/content/form2db/scripts/code.js
+++ sdkjs-plugins/content/form2db/scripts/code.js
@@ -4,15 +4,20 @@
         // const oDocument = Api.GetDocument();
         // const allForms = oDocument.getAllForms();
         // console.log(allForms);
-        
+
         window.Asc.plugin.executeMethod('GetAllForms');
 
-        const allForms = window.Asc.plugin.onMethodReturn = (data) => {
-            return data
+        window.Asc.plugin.onMethodReturn = (data) => {
+            if (data) {
+                for (const id of data) {
+                    console.log(id);
+                    window.Asc.plugin.executeMethod('GetFormValue', id);
+                }
+            }
         };
-        console.log(allForms);
-        // window.Asc.plugin.onMethodReturn = () => {
-        //     console.log("Running from onmethodreturn", window.Asc.plugin.info)
-        // }
+
+        window.Asc.plugin.onMethodReturn = (data) => {
+            console.log('Hopefully this contains form values');
+        };
     };
 })(window, undefined);
