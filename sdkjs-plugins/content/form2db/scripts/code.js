2ec0303a793d7c5228036001a717883c27c7097b
Georg Kiviväli <georg.kivivali@uptime.eu>
2022-12-01 12:03:30 +0200

    changes

--- sdkjs-plugins/content/form2db/scripts/code.js
+++ sdkjs-plugins/content/form2db/scripts/code.js
@@ -1,21 +1,25 @@
 ((window, undefined) => {
     window.Asc.plugin.init = () => {
+        const ids = [];
+
         window.Asc.plugin.executeMethod('GetAllForms');
 
         window.Asc.plugin.onMethodReturn = (data) => {
             if (data) {
                 for (const id of data) {
-                    console.log(id);
-                    window.Asc.plugin.executeMethod('GetFormValue', id);
-
-                    window.Asc.plugin.onMethodReturn = (data) => {
-                        console.log(
-                            'Hopefully this contains form values',
-                            data
-                        );
-                    };
+                    ids.push(id);
                 }
             }
         };
+
+        for (const id of ids) {
+            console.log(id);
+
+            window.Asc.plugin.executeMethod('GetFormValue', id);
+
+            window.Asc.plugin.onMethodReturn = (data) => {
+                console.log('Hopefully this contains form values', data);
+            };
+        }
     };
 })(window, undefined);
