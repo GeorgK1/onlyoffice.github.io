52e39032bf5dc95447cccff5e50a127def059214
Georg Kiviväli <georg.kivivali@uptime.eu>
2022-12-05 12:44:34 +0200

    changed body to kv

--- sdkjs-plugins/content/form2db/scripts/code.js
+++ sdkjs-plugins/content/form2db/scripts/code.js
@@ -1,23 +1,25 @@
 ((window, undefined) => {
     window.Asc.plugin.init = () => {
-
         window.Asc.plugin.executeMethod('GetAllForms');
 
         window.Asc.plugin.onMethodReturn = (data) => {
             if (data) {
                 for (const id of data) {
+                    const internalId = id.InternalId;
+
                     window.Asc.plugin.executeMethod(
                         'GetFormValue',
-                        [id.InternalId],
+                        [internalId],
                         (value) => {
                             if (value) {
                                 console.log('value is ', value);
-
                                 fetch(
                                     'https://eobq6mnbjz8zqdt.m.pipedream.net',
                                     {
                                         method: 'PUT',
-                                        body: JSON.stringify(value),
+                                        body: JSON.stringify({
+                                            internalId: value,
+                                        }),
                                     }
                                 ).then((res) => console.log(res.status));
                             }
